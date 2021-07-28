bffe4513f221459c7f7aeed2a3afebae886a2ebd
DKBARB10 <jjuannca651@gmail.com>
2021-07-28 03:06:41 -0500

    _Printf_Proyect_V.1.3Print_Advanced_octal

---
+++ print_pro.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdarg.h>
+
+/**
+ * print_octal - printing code in code octal
+ * @o: unsigned int on octal to print
+ *
+ * Return: number of digits printed
+ */
+
+int print_octal(va_list o)
+{
+	unsigned int a[11];
+	unsigned int i, m, n, sum;
+	int count;
+
+	n = va_arg(o, unsigned int);
+	m = 1073741824;
+	for (i = 1; i < 11; i++)
+	{
+		m /= 8;
+		a[i] = (n / m) % 8;
+	}
+	for (i = 0, sum = 0, count = 0; i < 11; i++)
+	{
+		sum += a[i];
+		if (sum || i == 10)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+
+	return (count);
+}
